e0ef0066575732d4e353c6980d05bbf1eea7a5d6
joshin-run <jahmsc@gmail.com>
2017-05-02 11:47:39 -0600

    Feat (axios) Install/Import axios into Data.js; prep to API

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,47 +1,17 @@
 import React, { Component } from 'react'
-// import jQuery from '../scripts/jquery-3.2.1.min'
-// import $ from '../scripts/jquery-3.2.1.min'
+import axios from 'axios'
 
 class Data extends Component {
-
-  dataFlow () {
-
-  const whoIs = 'people/'
-  const url = 'http://swapi.co/api/' + whoIs
-
-  $.ajax({
-    url: url
-  }).done(function(data) {
-    console.log('The returned object is:', data)
-
-    if ( whoIs === 'people/') {
-      const results = data.results;
-      const resultsLength = results.length;
-      const people = [];
-
-      for (let i = 0; i < resultsLength; i++) {
-        people.push(results[i].name)
-      }
-
-      console.log('the people are:', people);
-    }
-    else {
-      console.log('the person is:', data.name)
-    }
-  })
-}
-
-
-  render() {
+  render () {
+    axios.get('http://swapi.co/api/whois')
+    .then(response => { console.log(response) })
 
     return (
-      <div className="data-stack">
-        <p className="data-title">Data</p>
+      <div className='data-stack'>
+        <p className='data-title'>Data</p>
         <button>Data</button>
       </div>
     )
   }
 }
-
-
 module.exports = Data
